b63d2e3e26b083dafaad364a3c0769c281987651
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-04-24 12:37:31 -0700

    added feedback and dynamic mode

--- src/components/DefogDynamicViz.js
+++ src/components/DefogDynamicViz.js
@@ -4,52 +4,68 @@ import PieChart from "./Charts/PieChart.jsx";
 import ColumnChart from './Charts/ColumnChart.jsx';
 import TrendChart from './Charts/TrendChart.jsx';
 
-const DefogDynamicViz = ({vizType, response, rawData, query}) => {
-  return (vizType === "table") ? <Table
-    dataSource={response.data}
-    columns={response.columns}
-    style={{
-      maxHeight: 300,
-      overflow: "auto",
-    }}
-    size="small"
-    pagination={{ pageSize: 5}}
-  /> : 
-  (vizType === "piechart") ? <PieChart
-    resourceId={null}
-    data={{
-      dataJSON: rawData.map((el) => ({ name: el[0], y: el[1] })),
-      title: query,
-    }}
-    height={400}
-    standaloneChart={false}
-    logo={null}
-    noTitle={false}
-  /> :
-  (vizType === "columnchart") ? <ColumnChart
-    resourceId={null}
-    data={{
-      dataJSON: rawData.map((el) => el[1]),
-      xAxisCategories: rawData.map((el) => el[0]),
-      title: query,
-    }}
-    height={400}
-    standaloneChart={false}
-    logo={null}
-    noTitle={false}
-  /> : 
-  (vizType === "trendchart") ? <TrendChart
-    resourceId={null}
-    data={{
-      dataJSON: rawData,
-      title: query,
-    }}
-    transformMode="base"
-    height={400}
-    standaloneChart={false}
-    logo={null}
-    noTitle={false}
-  /> : null
+const DefogDynamicViz = ({vizType, response, rawData, query, debugMode}) => {
+  let results;
+  if (vizType === "table") {
+    results = <Table
+      dataSource={response.data}
+      columns={response.columns}
+      style={{
+        maxHeight: 300,
+        overflow: "auto",
+      }}
+      size="small"
+      pagination={{ pageSize: 5}}
+    />
+  } else if (vizType === "piechart") {
+    results = <PieChart
+      resourceId={null}
+      data={{
+        dataJSON: rawData.map((el) => ({ name: el[0], y: el[1] })),
+        title: query,
+      }}
+      height={400}
+      standaloneChart={false}
+      logo={null}
+      noTitle={false}
+    />
+  } else if (vizType === "columnchart") {
+    results = <ColumnChart
+      resourceId={null}
+      data={{
+        dataJSON: rawData.map((el) => el[1]),
+        xAxisCategories: rawData.map((el) => el[0]),
+        title: query,
+      }}
+      height={400}
+      standaloneChart={false}
+      logo={null}
+      noTitle={false}
+    />
+  } else if (vizType === "trendchart") {
+    results = <TrendChart
+      resourceId={null}
+      data={{
+        dataJSON: rawData,
+        title: query,
+      }}
+      transformMode="base"
+      height={400}
+      standaloneChart={false}
+      logo={null}
+      noTitle={false}
+    />
+  }
+
+  return <div>    
+    {results}
+    {debugMode && <div className="rateQualityContainer">
+      <p>How did we do with is this query?</p>
+      <button style={{backgroundColor: "#fff"}} onClick={() => console.log("Good")}>👍 Great </button>
+      <button style={{backgroundColor: "#fff"}} onClick={() => console.log("Bad")}>👎 Bad </button>
+    </div>
+    }
+  </div>
 }
 
 export default React.memo(DefogDynamicViz, () => true);
