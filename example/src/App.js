5dd32262e417b40f24afbef4626987da043eb183
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-05-20 02:56:38 +0800

    added experimental personality fields

--- example/src/App.js
+++ example/src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
-
 import { AskDefogChat } from "defog-react";
-// import 'defog-react/dist/index.css'
 
 const App = () => {
   return (
     <AskDefogChat
       maxWidth={"100%"}
       maxHeight={"100%"}
-      //apiEndpoint="ws://127.0.0.1:8000/ws"
       apiEndpoint="http://127.0.0.1:8000/generate_query_chat"
       buttonText={"Ask Defog"}
-      //mode={"websocket"}
-      mode={"http"}
-      debugMode={true}
+      personality={"irreverent, sarcastic, witty"}
+      // mode={"websocket"}
+      // debugMode={true}
       // apiKey={"1"}
     />
   );
