1f85db17b2be05d3974ebe1d85eb67b196015ae8
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-05-25 00:08:44 +0800

    updated example endpoint

--- example/src/App.js
+++ example/src/App.js
@@ -6,12 +6,13 @@ const App = () => {
     <AskDefogChat
       maxWidth={"100%"}
       maxHeight={"100%"}
-      apiEndpoint="https://test-defog-ikcpfh5tva-uc.a.run.app"
+      apiEndpoint="http://127.0.0.1:8000/generate_query_chat"
+      // apiEndpoint="https://test-defog-ikcpfh5tva-uc.a.run.app"
       buttonText={"Ask Defog"}
       personality={"sarcastic and aloof"}
       // mode={"websocket"}
-      // debugMode={true}
-      // apiKey={"1"}
+      debugMode={true}
+      apiKey={"test"}
     />
   );
 };
