8070ca4af0c7acf3edb7c8430436ff55f6e1c662
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-05-31 18:13:22 +0800

    updated example app

--- example/src/App.js
+++ example/src/App.js
@@ -3,18 +3,28 @@ import { AskDefogChat } from "defog-react";
 
 const App = () => {
   return (
-    <AskDefogChat
-      maxWidth={"100%"}
-      maxHeight={"100%"}
-      // apiEndpoint="http://127.0.0.1:8000/generate_query_chat"
-      apiEndpoint="https://test-defog-ikcpfh5tva-uc.a.run.app"
-      buttonText={"Ask Defog"}
-      personality={"sarcastic and aloof"}
-      // mode={"websocket"}
-      debugMode={true}
-      apiKey={"test"}
-      darkMode={true}
-    />
+    <div style={{
+      display: "flex",
+      flexDirection: "row",
+    }}>
+      <div style={{width: "30%"}}>
+        <h1>Testing Defog</h1>
+      </div>
+      <div style={{width: "40%"}}>
+        <AskDefogChat
+          maxWidth={"100%"}
+          maxHeight={600}
+          // apiEndpoint="http://127.0.0.1:8000/generate_query_chat"
+          apiEndpoint="https://test-defog-ikcpfh5tva-uc.a.run.app"
+          buttonText={"Ask Defog"}
+          personality={"sarcastic and aloof"}
+          // mode={"websocket"}
+          debugMode={true}
+          apiKey={"test"}
+          darkMode={true}
+        />
+      </div>
+    </div>
   );
 };
 
