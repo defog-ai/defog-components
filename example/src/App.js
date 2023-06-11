7e33446653867f2724a76185ce13dd94fe7da0c6
Manas Sharma <manasdotsharma@gmail.com>
2023-06-11 13:21:05 +0530

    dummy

--- example/src/App.js
+++ example/src/App.js
@@ -14,12 +14,20 @@ const App = () => {
           maxWidth={"100%"}
           maxHeight={500}
           apiEndpoint="https://test-defog-ikcpfh5tva-uc.a.run.app"
+          // apiEndpoint="http://127.0.0.1:8080/generate_query_chat"
+          // apiEndpoint="https://defog-gcp-ktcmdcmg4q-uc.a.run.app"
           buttonText={"Ask Defog"}
           personality={"sarcastic and aloof"}
           debugMode={true}
           apiKey={"test"}
           sqlOnly={false}
-          // additionalParams={{ "test": "test" }}
+          additionalParams={
+            {
+              // api_key:
+              //   "",
+              // db_type: "",
+            }
+          }
           // additionalHeaders={{ "test": "test" }}
         />
       </div>
