019404b53c8030159866f8a7de39fe5f1e6fc359
Manas Sharma <manasdotsharma@gmail.com>
2023-08-25 15:22:25 +0530

    fix table rendering

--- src/components/agent/WriterGroup.jsx
+++ src/components/agent/WriterGroup.jsx
@@ -3,11 +3,26 @@
 
 import React, { Fragment, useState } from "react";
 import Writer from "./Writer";
+import { createRoot } from "react-dom/client";
 
 export default function WriterGroup({ items }) {
   const [active, setActive] = useState(0);
   function onComplete() {
     setActive(active + 1);
+    try {
+      if (items[active] && items[active]?.type === "csvTable") {
+        const table = items[active];
+        const id = table.emptyHtml.match(/csv-table-[0-9]+/)[0];
+        // find the matching id in window.renders
+        const rdr = window.renders.find((d) => d.id === id);
+
+        const Component = rdr.component;
+        const root = createRoot(document.getElementById(id));
+        root.render(<Component {...rdr.props} apiKey={""} apiEndpoint={""} />);
+      }
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
