b53b1fde9e701b1b70ef35e1fb5460517f0ab3fa
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-11-07 15:41:52 +0800

    added helpful comments

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -25,6 +25,7 @@ const Answers = ({
   }
   
   useEffect(() => {
+    // Convert questionsAsked object into an array of answer objects
     const answers = Object.keys(questionsAsked).map((questionId) => {
       const { question, sql, level, parentQuestionId, askedAt, columns, data } = questionsAsked[questionId];
       return {
@@ -38,13 +39,20 @@ const Answers = ({
         data,
       }
     });
+
+    // Now, we want to sort answers so that it's like the following:
+    // - all answers with level = 0 are sorted in ascending order of askedAt
+    // - any answers that are children of a given answer are included right after their parent answer
+
+    // First, sort the level 0 answers by their 'askedAt' ISOstring in ascending order
     answers.sort((a, b) => {
       if (a.level === 0 && b.level === 0) {
         return a.askedAt > b.askedAt;
       }
-      return 0;
+      return 0; // Keep the original order if levels are not 0
     });
 
+    // Create a mapping from parentQuestionId to its corresponding child answers
     const parentToChildrenMap = answers.reduce((map, answer) => {
       if (answer.parentQuestionId !== undefined) {
         if (!map[answer.parentQuestionId]) {
@@ -55,13 +63,16 @@ const Answers = ({
       return map;
     }, {});
     
+    // Recursive function to build the sorted answer list according to the hierarchy
     const getOrderedAnswers = (answerList, parentId) => {
       let orderedAnswers = [];
       for (const answer of answerList) {
+        // Check if the current answer is a child of the given parentId or is a level 0 answer
         if ((answer.parentQuestionId === parentId && answer.level !== 0) || (parentId === undefined && answer.level === 0)) {
           orderedAnswers.push(answer);
           // If this answer has children, add them right after the answer
           if (parentToChildrenMap[answer.questionId]) {
+            // Recursively get the children answers and add them to the list
             const children = getOrderedAnswers(parentToChildrenMap[answer.questionId], answer.questionId);
             orderedAnswers = orderedAnswers.concat(children);
           }
